c580f83b010ce617aab065adb290a881a7d330b0
LeeGeunmo <rmsah0105@naver.com>
2024-06-03 23:17:16 +0900

    community anonymous 접근제한

--- apps/community/views.py
+++ apps/community/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from ..group.models import Group
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required(login_url='/user/login/')
 def main(request):
     user = request.user
     group = Group.objects.filter(members=user)
