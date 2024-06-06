b9aba6b09201fded017e752ab857c1dcc385019f
ynsyeon <seoe1009@naver.com>
2024-06-06 21:01:11 +0900

    Save post1

--- apps/post/views.py
+++ apps/post/views.py
@@ -11,52 +11,44 @@ def main(request):
     print(posts)
     return render(request, 'community/main.html', {"posts": posts})
 
-def create_post(request) :
-  
-    if request.method=='POST':
-        content=request.POST.get('content')
+def create_post(request):
+    if request.method == 'POST':
+        content = request.POST.get('content')
 
-        #게시글 생성
-        new_post=Post.objects.create(
-            user=request.user,
-            content=content,
-            created_time=timezone.now(),
-        )
-        new_post.save()
-        posts = Post.objects.all()
-        return redirect('community:main',{"posts": posts})
+        # 게시글 생성
+        if content:
+            Post.objects.create(content=content, user=request.user)
+        
+        return redirect('community:main')
+    
     posts = Post.objects.all()
-    return render(request,'community/main.html',{"posts": posts})
+    return render(request, 'community/main.html', {"posts": posts})
 
 def edit_post(request, post_id) :
     post=get_object_or_404(Post,pk=post_id)
 
     if request.method=='POST':
         #요청에서 데이터 추출
-        title=request.POST.get('title')
-        content=request.POST.get('content')
+        content = request.POST.get('content')
 
-        #게시글 정보 업데이트
-        post.title=title
-        post.content=content
-        post.updated_time=timezone.now()
-        post.save()
-        posts = Post.objects.all()
-        return redirect('community:main',{"posts": posts})
-    posts = Post.objects.all()
-    return render(request, 'community/main.html',{"posts": posts})
+        if content:
+            post.content = content
+            post.save()
+        
+        return redirect('community:main')
+    return render(request, 'community/edit_post.html', {"post": post})
 
 def delete_post(request,post_id) :
     post=get_object_or_404(Post, pk=post_id)
 
-    if request.method=='POST':
+    if request.method == 'POST':
         post.delete()
-        posts = Post.objects.all()
-        return redirect('community:main',{"posts": posts})
-    posts = Post.objects.all()
-    return render(request,'community/main.html',{"posts": posts})
+        return redirect('community:main')
+    return render(request, 'community/delete_post.html', {"post": post})
 
 def post_list(request):
     #게시글 리스트 가져오기
     posts=Post.objects.all()
+    print(posts)
     return render(request, 'community/main.html', {"posts": posts})
+
